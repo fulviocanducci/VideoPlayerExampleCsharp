77bf85c4e3562cdf02051503556d261f9fe9a4f9
fulvio <fulviocanducci@hotmail.com>
2025-06-01 12:20:18 -0300

    update now!@

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GrpBoxVideo = New GroupBox()
+        SuspendLayout()
+        ' 
+        ' GrpBoxVideo
+        ' 
+        GrpBoxVideo.Location = New Point(40, 28)
+        GrpBoxVideo.Name = "GrpBoxVideo"
+        GrpBoxVideo.Size = New Size(683, 386)
+        GrpBoxVideo.TabIndex = 0
+        GrpBoxVideo.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GrpBoxVideo)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GrpBoxVideo As GroupBox
+
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports LibVLCSharp.Shared
+Imports LibVLCSharp.WinForms
+Public Class Form1
+    Private LibVLC As LibVLC
+    Private MediaPlayer As MediaPlayer
+    Private VideoView As VideoView
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LibVLC = New LibVLC()
+        MediaPlayer = New MediaPlayer(LibVLC)
+        VideoView = New VideoView() With {
+            .Dock = DockStyle.Fill,
+            .MediaPlayer = MediaPlayer
+        }
+        GrpBoxVideo.Controls.Add(VideoView)
+        Dim videoPath = "C:\Temp\Videos\1.mp4"
+        Using media As Media = New Media(LibVLC, videoPath, FromType.FromPath)
+            MediaPlayer.Play(media)
+        End Using
+    End Sub
+End Class
